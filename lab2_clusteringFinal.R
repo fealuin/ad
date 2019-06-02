2645c41f7d387fed54e12e208bff3a4738d807dc
jgiglio <jgiglio@scainventarios.cl>
2019-06-02 12:55:10 -0400

    Limpieza, comentarios, graficos

--- lab2_clusteringFinal.R
+++ lab2_clusteringFinal.R
@@ -6,10 +6,9 @@ library(fpc)
 library(NbClust)
 library(clValid)
 library(optpart)
-
-library("cluster") # Instalar
-library('clusteval') # Instalar
-library('clues') # install.packages("clues")
+library(cluster) 
+library(clusteval) 
+library(clues) 
 
 
 # Normaliza entre 0 y 1 
@@ -17,6 +16,7 @@ normalize <- function(x) {
   return ((x - min(x)) / (max(x) - min(x)))
 }
 
+# Reduce las clases a 2: calidad >=6 bueno <6 malo
 reClass <- function(x) {
   
   return (sapply(x,function(x){
@@ -29,74 +29,63 @@ reClass <- function(x) {
   }))
 }
 
-# cantidad de grupos
-k = 2
+# Cantidad de clusters
+k <- 2
+
+# Distancia a utilizar
+distance <- 'euclidean'
+
+# Método de clustering
+clustFUN <- 'kmeans'
 
 # Lectura de archivos
 wineWhite<-read.csv('./datasets/winequality-white.csv',sep=';')
 
-#Se reducen las clases a 4: < 4, < 5 y >= 4,  < 6 y >= 5, > 6 
+## Preprocsamiento
+
+# Reducción de clases 
 wineWhite$quality<-reClass(wineWhite$quality)
 
+# Se deja la calidad como factores
 wineWhite.q<-as.numeric(as.factor(paste0('q' , as.character(wineWhite$quality))))
 
 # Estandarización
-wineWhite.norm<-as.data.frame(scale(wineWhite[,-ncol(wineWhite)]))
-#Cálculo de distancia
-wineWhite.dist<-Dist(wineWhite.norm,method='euclidean')
+wineWhite.std<-as.data.frame(scale(wineWhite[,-ncol(wineWhite)]))
 
-# wineWhite.pam<-pamk(wineWhite.norm,krange = 2:7)
-# Cálculos de pam (eclust)
-wineWhite.pam<-eclust(wineWhite.norm,FUNcluster = 'pam',hc_metric='euclidean',graph = FALSE,k)
+# Normalizacion
+wineWhite.norm<-as.data.frame(lapply(wineWhite[,-ncol(wineWhite)],normalize))
 
-# optimizacion de silueta
-# wineWhite.optsil<-optsil(wineWhite.pam$clustering,wineWhite.dist,100)
 
-# fviz_cluster(wineWhite.pam,geom='point',show.clust.cent = TRUE,ggtheme = theme_minimal(),ellipse.type = 'norm')
-# fviz_silhouette(wineWhite.pam)
+## Determinación de k en base a índice silueta medio
 
-# Medidas de calidad
-wineWhite.pam.cluster.stats<-cluster.stats(wineWhite.dist,wineWhite.q,as.vector(wineWhite.pam$clustering))
+#kmeans
+fviz_nbclust(wineWhite.norm, kmeans, method = "silhouette")
+
+# Cálculo de distancia
+wineWhite.dist<-Dist(wineWhite.norm,method=distance)
+
+# Cálculos de clúster
+wineWhite.clustering<-eclust(wineWhite.std,FUNcluster = clustFUN,hc_metric=distance,graph = FALSE,k=k)
+
+# Plots clustering
+fviz_cluster(wineWhite.clustering,geom='point',show.clust.cent = TRUE,ggtheme = theme_minimal(),ellipse.type = 'norm')
+fviz_silhouette(wineWhite.clustering)
+
+## Métricas de calidad
 
 # Matriz de confusión
-table(wineWhite$quality,wineWhite.pam$clustering)
+table(wineWhite$quality,wineWhite.clustering$cluster)
 
+# Internas
+wineWhite.clustering.stats<-cluster.stats(wineWhite.dist,wineWhite.q,as.vector(wineWhite.clustering$cluster))
+
+
+# Externas
+  
 # jaccard
 # estructura de cluster para cluster inicial
 clusterPropuesto <- wineWhite$quality
 names(clusterPropuesto) <- rownames(wineWhite)
-
-jaccard=round(cluster_similarity(wineWhite.pam$clustering,clusterPropuesto, similarity = 'jaccard', method = "independence"),6)
+jaccard=round(cluster_similarity(wineWhite.clustering$cluster,clusterPropuesto, similarity = 'jaccard', method = "independence"),6)
 print(jaccard)
 
-
-
-
-# Cálculos de pam (eclust)
-# wineWhite.metodo<-eclust(wineWhite.norm,FUNcluster = 'pam',hc_metric='euclidean',graph = FALSE,k)
-# set.seed(123)
-# km.res <- kmeans(df, k, nstart = 25)
-wineWhite.kmeans = kmeans(as.dist(wineWhite.dist), k)
-
-# Average silhouette for kmeans
-fviz_nbclust(wineWhite.norm, pam, method = "silhouette")
-
-# optimizacion de silueta
-# wineWhite.optsil<-optsil(wineWhite.pam$clustering,wineWhite.dist,100)
-
-# fviz_cluster(wineWhite.kmeans,geom='point',show.clust.cent = TRUE,ggtheme = theme_minimal(),ellipse.type = 'norm')
-# fviz_silhouette(wineWhite.kmeans)
-
-# Medidas de calidad
-wineWhite.kmeans.cluster.stats<-cluster.stats(wineWhite.dist,wineWhite.q,as.vector(wineWhite.kmeans$cluster))
-
-# Matriz de confusión
-table(wineWhite$quality,wineWhite.kmeans$cluster)
-
-# jaccard
-# estructura de cluster para cluster inicial
-clusterPropuesto <- wineWhite$quality
-names(clusterPropuesto) <- rownames(wineWhite)
-
-jaccard=round(cluster_similarity(wineWhite.kmeans$cluster,clusterPropuesto, similarity = 'jaccard', method = "independence"),6)
-print(jaccard)
