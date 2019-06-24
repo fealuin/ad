f25f28fb4dfba540bba757da6a3c8132fb87e6a2
jgiglio <jgiglio@scainventarios.cl>
2019-06-23 20:04:06 -0400

    ROC init

--- lab4.R
+++ lab4.R
@@ -4,6 +4,7 @@ library(caret)
 library(survey)
 library(lmtest)
 library(pROC)
+library(precrec)
 
 wineWhite<-read.csv('./datasets/winequality-white.csv',sep=';')
 reClass <- function(x) {
@@ -17,11 +18,19 @@ reClass <- function(x) {
     }
   }))
 }
+## Preproceso
+
+# Separar vino en 2 clases 
 wineWhite$quality<-factor(reClass(wineWhite$quality))
+# Estandarización
+wineWhite.std<-as.data.frame(cbind(scale(wineWhite[,-ncol(wineWhite)]),wineWhite$quality))
+
+names(wineWhite.std)<-names(wineWhite)
+wineWhite.std$quality<-factor(wineWhite.std$quality)
 
 #ejemplo de https://stats.idre.ucla.edu/r/dae/logit-regression/
 
-reg<-glm(quality~fixed.acidity+volatile.acidity+citric.acid+residual.sugar+chlorides+free.sulfur.dioxide+total.sulfur.dioxide+density+ pH+sulphates+alcohol,data=wineWhite,family = 'binomial')
+reg<-glm(quality~fixed.acidity+volatile.acidity+citric.acid+residual.sugar+chlorides+free.sulfur.dioxide+total.sulfur.dioxide+density+ pH+sulphates+alcohol,data=wineWhite.std,family = 'binomial')
 
 
 #Significancia
@@ -35,18 +44,28 @@ reg$coefficients
 
 #Predict
 plot()
-predict(reg,newdata=wineWhite)
+predict(reg,newdata=wineWhite.std)
 #Significancia
 logLik(reg)
 
 #Importancia de variables
 varImp(reg)
 
+
+
+autoplot(evalmod(scores=f1))
+
+# ROC
+prob<-predict(reg,type=c('response'))
+f1 = roc(quality ~ prob, data=wineWhite.std) 
+plot(f1, col="red")
+
+
 # K Fold
 set.seed(1)
 Train <- createDataPartition(wineWhite$quality, p=0.6, list=FALSE)
-training <- wineWhite[ Train, ]
-testing <- wineWhite[ -Train, ]
+training <- wineWhite.std[ Train, ]
+testing <- wineWhite.std[ -Train, ]
 
 ctrl <- trainControl(method = "repeatedcv", number = 10, savePredictions = TRUE)
 
@@ -55,3 +74,10 @@ mod_fit <- train(quality~fixed.acidity+volatile.acidity+citric.acid+residual.sug
 
 pred = predict(mod_fit, newdata=testing)
 confusionMatrix(data=pred, testing$quality)
+
+
+
+
+
+
+
