fc6aeb8b032b2a5cee6714f3bc2d429a264c7c5f
jgiglio <jgiglio@scainventarios.cl>
2019-06-30 17:52:59 -0400

    Discretize

--- pep3.R
+++ pep3.R
@@ -10,11 +10,34 @@ data=readARFF("./datasets/credit_fruad.arff")
 data.numeric=read.csv('./datasets/proc_german_num_02 withheader-2.tab.tsv',sep = '\t')
 
 D<-daisy(data[,-21],metric='gower')
-mds<-as.data.frame(cmdscale(D))
-names(mds)<-c('x','y')
-plot_ly(data=mds,x=~x,y=~y,type = 'scatter',color=data[,21])
 
-discretizeDF(data,breaks=10,methods='frecuency')
+
+#mds<-as.data.frame(cmdscale(D))
+#names(mds)<-c('x','y')
+#plot_ly(data=mds,x=~x,y=~y,type = 'scatter',color=data[,21])
+
+#discretizeDF(data.numeric)
+
+#Discretize
+toDis<-c(
+  'current_balance',
+  'credit_usage',
+  'cc_age')
+
+toFactor<-c(  
+  'location',
+  'residence_since',
+  'existing_credits',
+  'num_dependents'
+  )
+
+data.dis<-data
+
+
+
+data.dis[,toDis] <-discretizeDF(data[,toDis])
+data.dis[,toFactor]<-data.frame(as.factor(data$location),as.factor(data$residence_since),as.factor(data$existing_credits),as.factor(data$num_dependents))
+
 
 rules<-apriori(data,parameter = list(support=0.01,confidence=0.01,minlen=2),appearance=list(rhs=c('class=bad')))
 
